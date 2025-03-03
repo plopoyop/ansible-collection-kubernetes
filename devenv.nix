7eef6190f751f2018cf70482e4f72b2bfeac83f9
Clément Hubert <clement@plopoyop.me>
2025-03-03 14:29:04 +0100

    introduce devenv.sh

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.ANSIBLE_ROLES_PATH=".devenv/ansible/roles";
+  env.ANSIBLE_COLLECTIONS_PATH=".devenv/ansible/collections";
+  env.LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+  env.LANG="en_US.UTF8";
+
+  packages = [
+    pkgs.git
+    pkgs.python312Packages.kubernetes
+    pkgs.python312Packages.boto3
+    pkgs.python312Packages.molecule
+    pkgs.python312Packages.molecule-plugins
+    pkgs.ansible
+    pkgs.ansible-lint
+    pkgs.yamllint
+    pkgs.pre-commit
+    pkgs.glibcLocales
+    pkgs.go-task
+  ];
+
+  languages.ansible.enable = true;
+
+  enterShell = ''
+    mkdir -p $ANSIBLE_ROLES_PATH
+    mkdir -p $ANSIBLE_COLLECTIONS_PATH
+  '';
+}
