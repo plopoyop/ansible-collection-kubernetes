8c487b7af58a76cd4b5be14b317b26b0991ec42b
Clément Hubert <clement@plopoyop.me>
2025-03-06 06:56:41 +0100

    add kind cloud provider for tests

--- devenv.nix
+++ devenv.nix
@@ -11,6 +11,7 @@
     pkgs.git
     pkgs.glibcLocales
     pkgs.go-task
+    pkgs.jq
     pkgs.kind
     pkgs.pre-commit
     pkgs.python312Packages.kubernetes
@@ -21,6 +22,34 @@
   ];
 
   languages.ansible.enable = true;
+
+  scripts.install-kind-cloud.exec = ''
+    OSTYPE="$(uname -s | tr -d '0-9.' | tr '[:upper:]' '[:lower:]')"
+    CPUTYPE="$(uname -m)"
+    BIN_DEST=$DEVENV_DOTFILE/bin/
+    BIN_NAME="cloud-provider-kind"
+
+    if [[ -f $BIN_DEST/$BIN_NAME ]]; then
+      echo "already installed"
+      exit 0;
+    fi
+
+    case "$CPUTYPE" in
+      aarch64|arm64)
+        GH_CPUTYPE="arm64";;
+      x86_64)
+        GH_CPUTYPE="amd64" ;;
+      *)
+        echo '??' ;;
+    esac
+
+    KIND_CLOUD_SRC=$(curl -s https://api.github.com/repos/kubernetes-sigs/cloud-provider-kind/releases/latest  |  jq -r '.assets[] | select(.name | contains ("'$OSTYPE'")) | select(.name | contains ("'$GH_CPUTYPE'"))  | .browser_download_url')
+    echo $KIND_CLOUD_SRC
+
+    mkdir -p $BIN_DEST
+    curl -L $KIND_CLOUD_SRC | tar -xz -C $BIN_DEST $BIN_NAME
+  '';
+
 
   enterShell = ''
     export ANSIBLE_ROLES_PATH=$PWD/.devenv/ansible/roles;
